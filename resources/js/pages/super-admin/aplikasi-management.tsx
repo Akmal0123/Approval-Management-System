c85cbd41cf08a5feff92e2db90591790da887530
Fahmi <23430028.fahmi@tsu.ac.id>
2026-09-01 10:35:31 +0700

    update aplikasi management

--- resources/js/pages/super-admin/aplikasi-management.tsx
+++ resources/js/pages/super-admin/aplikasi-management.tsx
@@ -325,7 +325,7 @@ export default function SuperAdminAplikasiManagement() {
                                                 <SelectValue placeholder="Pilih perusahaan" />
                                             </SelectTrigger>
                                             <SelectContent>
-                                                {companies.map((company) => (
+                                                {companies?.map((company) => (
                                                     <SelectItem key={company.id} value={company.id.toString()} className="font-sans">
                                                         {company.name}
                                                     </SelectItem>
@@ -337,7 +337,7 @@ export default function SuperAdminAplikasiManagement() {
                                         <Button type="button" variant="outline" onClick={closeModal} disabled={submitting} className="font-sans">
                                             Batal
                                         </Button>
-                                        <Button type="submit" disabled={submitting} className="bg-red-600 font-sans hover:bg-red-700">
+                                        <Button type="submit" disabled={submitting} className="font-sans">
                                             {submitting ? 'Menyimpan...' : editingAplikasi ? 'Perbarui' : 'Simpan'}
                                         </Button>
                                     </div>
